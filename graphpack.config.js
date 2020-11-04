c950b170ef92f9c22d7c5424e305eb3911ddbec7
Joe Fleming <joe.fleming@gmail.com>
2020-11-04 11:11:05 -0700

    chore: add graphql-voyager

--- graphpack.config.js
+++ graphpack.config.js
@@ -1,11 +1,14 @@
 /* eslint import/no-extraneous-dependencies: 0 */
 const express = require('express');
+const { express: voyagerMiddleware } = require('graphql-voyager/middleware');
 
 const app = express();
 
 app.get('/hello', (req, res) => {
   res.send('Hello world!');
 });
+
+app.use('/voyager', voyagerMiddleware({ endpointUrl: '/graphql' }));
 
 module.exports = {
   server: {
