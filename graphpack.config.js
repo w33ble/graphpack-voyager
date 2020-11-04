7759dd6294810140baa48ee4fbc271902b950ebf
Joe Fleming <joe.fleming@gmail.com>
2020-11-04 11:08:55 -0700

    chore: add custom express server

--- graphpack.config.js
+++ graphpack.config.js
@@ -1,7 +1,20 @@
+/* eslint import/no-extraneous-dependencies: 0 */
+const express = require('express');
+
+const app = express();
+
+app.get('/hello', (req, res) => {
+  res.send('Hello world!');
+});
+
 module.exports = {
   server: {
     introspection: true,
     playground: false,
     port: 8080,
+    applyMiddleware: {
+      app,
+      path: '/graphql',
+    },
   },
 };
